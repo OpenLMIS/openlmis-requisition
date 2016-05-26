c0c6fed02d3ce4bed5e11466264f0f9fcf82dac4
Chongsun Ahn <cahn922@yahoo.com>
2016-05-26 10:17:21 -0700

    Remove wildcard imports
    Frowned upon in Google Java Style Guide.

--- src/main/java/org/openlmis/requisition/domain/FacilityOperator.java
+++ src/main/java/org/openlmis/requisition/domain/FacilityOperator.java
@@ -4,7 +4,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "facility_operators")
--- src/main/java/org/openlmis/requisition/domain/User.java
+++ src/main/java/org/openlmis/requisition/domain/User.java
@@ -4,7 +4,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "users")
