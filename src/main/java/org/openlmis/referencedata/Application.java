OpenLMIS/openlmis-template-service

Add endpoint to get all i18n messages
Add support for getting all messages for i18n by adding a new REST endpoint. This new REST endpoint is necessary for clients, particularly for offline functionality.

For new implementation, the configuration requires basename to be classpath:messages, rather than simply messages.

src/main/java/org/openlmis/template/Application.java
@@ -1,9 +1,9 @@
 package org.openlmis.template;
 
+import org.openlmis.template.i18n.ExposedMessageSourceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -24,9 +24,11 @@ public class Application {
     }
 
     @Bean
-    public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
+    public ExposedMessageSourceImpl messageSource() {
+        ExposedMessageSourceImpl messageSource = new ExposedMessageSourceImpl();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
         return messageSource;
     }
 }

src/main/java/org/openlmis/template/i18n/ExposedMessageSource.java
@@ -1,0 +1,11 @@
+package org.openlmis.template.i18n;
+
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+import java.util.Map;
+
+public interface ExposedMessageSource extends MessageSource {
+
+    Map<String, String> getAllMessages(Locale locale);
+}

src/main/java/org/openlmis/template/i18n/ExposedMessageSourceImpl.java
@@ -1,0 +1,31 @@
+package org.openlmis.template.i18n;
+
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
+
+@Component
+public class ExposedMessageSourceImpl extends ReloadableResourceBundleMessageSource implements ExposedMessageSource {
+
+    protected Properties getAllProperties(Locale locale) {
+        clearCacheIncludingAncestors();
+        ReloadableResourceBundleMessageSource.PropertiesHolder propertiesHolder = getMergedProperties(locale);
+        return propertiesHolder.getProperties();
+    }
+
+    public Map<String, String> getAllMessages(Locale locale) {
+        Properties p = getAllProperties(locale);
+        Enumeration<String> keys = (Enumeration<String>) p.propertyNames();
+        Map<String, String> asMap = new HashMap<>();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            asMap.put(key, p.getProperty(key));
+        }
+        return asMap;
+    }
+}

src/main/java/org/openlmis/template/i18n/MessageService.java
@@ -1,0 +1,31 @@
+package org.openlmis.template.i18n;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+import java.util.Map;
+
+@Service
+public class MessageService {
+
+    @Autowired
+    private ExposedMessageSource messageSource;
+
+    @Setter
+    @Getter
+    private Locale currentLocale;
+
+    @Autowired
+    public MessageService(ExposedMessageSource messageSource) {
+        this.messageSource = messageSource;
+        this.currentLocale = Locale.ENGLISH;
+    }
+
+    public Map<String, String> getAllMessages() {
+        return messageSource.getAllMessages(currentLocale);
+    }
+}

OpenLMIS/openlmis-requisition

src/main/java/org/openlmis/referencedata/Application.java
@@ -1,9 +1,9 @@
 package org.openlmis.referencedata;
 
+import org.openlmis.referencedata.i18n.ExposedMessageSourceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -24,9 +24,11 @@ public class Application {
     }
 
     @Bean
-    public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
+    public ExposedMessageSourceImpl messageSource() {
+        ExposedMessageSourceImpl messageSource = new ExposedMessageSourceImpl();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
         return messageSource;
     }
 }

src/main/java/org/openlmis/referencedata/i18n/ExposedMessageSource.java
@@ -1,0 +1,11 @@
+package org.openlmis.referencedata.i18n;
+
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+import java.util.Map;
+
+public interface ExposedMessageSource extends MessageSource {
+
+    Map<String, String> getAllMessages(Locale locale);
+}

src/main/java/org/openlmis/referencedata/i18n/ExposedMessageSourceImpl.java
@@ -1,0 +1,31 @@
+package org.openlmis.referencedata.i18n;
+
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
+
+@Component
+public class ExposedMessageSourceImpl extends ReloadableResourceBundleMessageSource implements ExposedMessageSource {
+
+    protected Properties getAllProperties(Locale locale) {
+        clearCacheIncludingAncestors();
+        ReloadableResourceBundleMessageSource.PropertiesHolder propertiesHolder = getMergedProperties(locale);
+        return propertiesHolder.getProperties();
+    }
+
+    public Map<String, String> getAllMessages(Locale locale) {
+        Properties p = getAllProperties(locale);
+        Enumeration<String> keys = (Enumeration<String>) p.propertyNames();
+        Map<String, String> asMap = new HashMap<>();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            asMap.put(key, p.getProperty(key));
+        }
+        return asMap;
+    }
+}

src/main/java/org/openlmis/referencedata/i18n/MessageService.java
@@ -1,0 +1,31 @@
+package org.openlmis.referencedata.i18n;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+import java.util.Map;
+
+@Service
+public class MessageService {
+
+    @Autowired
+    private ExposedMessageSource messageSource;
+
+    @Setter
+    @Getter
+    private Locale currentLocale;
+
+    @Autowired
+    public MessageService(ExposedMessageSource messageSource) {
+        this.messageSource = messageSource;
+        this.currentLocale = Locale.ENGLISH;
+    }
+
+    public Map<String, String> getAllMessages() {
+        return messageSource.getAllMessages(currentLocale);
+    }
+}