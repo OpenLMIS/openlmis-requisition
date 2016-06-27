b5ae2be98b144cc4875cd8a7a0546cedb29c45dd
Jakub Kondrat <jkondrat@soldevelo.com>
2016-06-27 14:24:14 +0200

    Fixing Facility API

--- src/main/java/org/openlmis/referencedata/repository/FacilityTypeRepository.java
+++ src/main/java/org/openlmis/referencedata/repository/FacilityTypeRepository.java
@@ -1,9 +1,17 @@
 package org.openlmis.referencedata.repository;
 
 import org.openlmis.referencedata.domain.FacilityType;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.UUID;
 
 public interface FacilityTypeRepository extends ReferenceDataRepository<FacilityType, UUID> {
-    //Add custom FacilityType related members here. See UserRepository.java for examples.
+
+  @Override
+  @RestResource
+  <S extends FacilityType> S save(S entity);
+
+  @Override
+  @RestResource
+  <S extends FacilityType> Iterable<S> save(Iterable<S> entities);
 }
