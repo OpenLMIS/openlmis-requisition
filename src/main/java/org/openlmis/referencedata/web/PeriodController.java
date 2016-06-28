617225cf3f72859b259d9fbe0f148b3d7f7454a5
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2016-06-28 13:14:23 +0200

    OLMIS-755: Remove @RestController annotation

--- src/main/java/org/openlmis/referencedata/web/PeriodController.java
+++ src/main/java/org/openlmis/referencedata/web/PeriodController.java
@@ -17,7 +17,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
-@RestController
 @RepositoryRestController
 public class PeriodController
 {
