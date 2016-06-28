6739cf701513319321c969e04625f6eec2f2b0e7
plal <pawellal@gmail.com>
2016-06-28 15:29:00 +0200

    Fixed imports

--- src/main/java/org/openlmis/referencedata/domain/RequisitionTemplate.java
+++ src/main/java/org/openlmis/referencedata/domain/RequisitionTemplate.java
@@ -4,8 +4,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.OneToOne;
+import javax.persistence.CascadeType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ElementCollection;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.Column;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 
 @Entity
 @Table(name = "requisitionTemplates")
