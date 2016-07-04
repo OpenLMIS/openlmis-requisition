6bb5d9548ec7d510477445be4c773040cf0570bc
plal <pawellal@gmail.com>
2016-07-04 15:44:42 +0200

    OLMIS-386: Replaced update loop with appropriate Repository function

--- src/main/java/org/openlmis/referencedata/repository/RequisitionTemplateRepository.java
+++ src/main/java/org/openlmis/referencedata/repository/RequisitionTemplateRepository.java
@@ -1,10 +1,13 @@
 package org.openlmis.referencedata.repository;
 
+import org.openlmis.referencedata.domain.Program;
 import org.openlmis.referencedata.domain.RequisitionTemplate;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface RequisitionTemplateRepository
         extends ReferenceDataRepository<RequisitionTemplate, UUID> {
-  //Add custom RequisitionTemplate related members here. See UserRepository.java for examples.
+  //Accessible via http://127.0.0.1:8080/api/requisitionTemplates/search/findByProgram?program=http://127.0.0.1:8080/api/programs/{programid}
+  RequisitionTemplate findByProgram(@Param("program") Program programid);
 }
--- src/main/java/org/openlmis/referencedata/web/RequisitionTemplateController.java
+++ src/main/java/org/openlmis/referencedata/web/RequisitionTemplateController.java
@@ -35,16 +35,12 @@ public class RequisitionTemplateController {
       // Ignore provided id
       requisitionTemplate.setId(null);
 
-      Iterable<RequisitionTemplate> it = requisitionTemplateRepository.findAll();
-
-      for (RequisitionTemplate template : it) {
-        if (requisitionTemplate.getProgram().getId().equals(
-              template.getProgram().getId())) {
-          requisitionTemplateRepository.delete(template);
-          logger.debug("Overwriting existing template");
-          break;
-        }
+      RequisitionTemplate it = requisitionTemplateRepository.findByProgram(
+              requisitionTemplate.getProgram());
+      if (it != null) {
+        requisitionTemplateRepository.delete(it);
       }
+      
       RequisitionTemplate newRequisitionTemplate =
               requisitionTemplateRepository.save(requisitionTemplate);
       return new ResponseEntity<RequisitionTemplate>(
