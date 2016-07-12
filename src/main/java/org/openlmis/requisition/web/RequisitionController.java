eba0cbbdb81651923d2f5314d4d997f560c2cc06
Grzegorz Araminowicz <garaminowicz@soldevelo.com>
2016-07-12 12:32:52 +0200

    OLMIS-229: View requisitions based on selected facility, program, and date filters

--- src/main/java/org/openlmis/requisition/web/RequisitionController.java
+++ src/main/java/org/openlmis/requisition/web/RequisitionController.java
@@ -1,5 +1,7 @@
 package org.openlmis.requisition.web;
 
+import org.openlmis.referencedata.domain.Facility;
+import org.openlmis.referencedata.domain.Program;
 import org.openlmis.requisition.domain.Requisition;
 import org.openlmis.requisition.domain.RequisitionStatus;
 import org.openlmis.requisition.repository.RequisitionRepository;
@@ -10,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -17,9 +20,19 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 @RepositoryRestController
 public class RequisitionController {
@@ -34,6 +47,9 @@ public class RequisitionController {
 
   @Autowired
   RequisitionService requisitionService;
+
+  @PersistenceContext
+  EntityManager entityManager;
 
   /**
    * Submits earlier initiated requisition.
@@ -55,6 +71,43 @@ public class RequisitionController {
         return new ResponseEntity(getRequisitionErrors(bindingResult), HttpStatus.BAD_REQUEST);
       }
     }
+  }
+
+  /**
+   * Finds requisitions matching all of provided parameters.
+   */
+  @RequestMapping(value = "/requisitions/search", method = RequestMethod.GET)
+  public ResponseEntity<?> searchRequisitions(
+      @RequestParam(value = "facility", required = false) Facility facility,
+      @RequestParam(value = "program", required = false) Program program,
+      @RequestParam(value = "createdDateFrom", required = false)
+      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime createdDateFrom,
+      @RequestParam(value = "createdDateTo", required = false)
+      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime createdDateTo) {
+    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Requisition> query = builder.createQuery(Requisition.class);
+    Root<Requisition> root = query.from(Requisition.class);
+
+    Predicate predicate = builder.conjunction();
+    if (facility != null) {
+      predicate = builder.and(predicate, builder.equal(root.get("facility"), facility));
+    }
+    if (program != null) {
+      predicate = builder.and(predicate, builder.equal(root.get("program"), program));
+    }
+    if (createdDateFrom != null) {
+      predicate = builder.and(predicate,
+          builder.greaterThanOrEqualTo(root.get("createdDate"), createdDateFrom));
+    }
+    if (createdDateTo != null) {
+      predicate = builder.and(predicate,
+          builder.lessThanOrEqualTo(root.get("createdDate"), createdDateTo));
+    }
+
+    query.where(predicate);
+    List<Requisition> result = entityManager.createQuery(query).getResultList();
+
+    return new ResponseEntity<>(result, HttpStatus.OK);
   }
 
   /**
