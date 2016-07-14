a9b20b79255d7f644389ab0d412bc6da531f8793
Pawel Muchowski <pmuchowski@soldevelo.com>
2016-07-14 15:37:01 +0200

    OLMIS-806: Improve IDL Related Functionality

---
+++ src/integration-test/java/org/openlmis/referencedata/web/ScheduleControllerIntegrationTest.java
@@ -0,0 +1,88 @@
+package org.openlmis.referencedata.web;
+
+import com.jayway.restassured.RestAssured;
+import guru.nidi.ramltester.RamlDefinition;
+import guru.nidi.ramltester.RamlLoaders;
+import guru.nidi.ramltester.junit.RamlMatchers;
+import guru.nidi.ramltester.restassured.RestAssuredClient;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openlmis.Application;
+import org.openlmis.referencedata.domain.Period;
+import org.openlmis.referencedata.domain.Schedule;
+import org.openlmis.referencedata.repository.PeriodRepository;
+import org.openlmis.referencedata.repository.ScheduleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+@WebIntegrationTest("server.port:8080")
+public class ScheduleControllerIntegrationTest {
+  private static final String BASE_URL = "http://localhost:8080";
+  private static final String RAML_ASSERT_MESSAGE = "HTTP request/response should match RAML definition.";
+
+  @Autowired
+  private ScheduleRepository scheduleRepository;
+
+  @Autowired
+  private PeriodRepository periodRepository;
+
+  private RamlDefinition ramlDefinition;
+  private RestAssuredClient restAssured;
+
+  private Schedule schedule;
+  private Period period;
+
+  @Before
+  public void setUp() {
+    RestAssured.baseURI = BASE_URL;
+    ramlDefinition = RamlLoaders.fromClasspath().load("api-definition.yaml");
+    restAssured = ramlDefinition.createRestAssured();
+
+    cleanup();
+
+    schedule = new Schedule();
+    schedule.setCode("code");
+    schedule.setName("schedule");
+    schedule.setDescription("Test schedule");
+    scheduleRepository.save(schedule);
+
+    period = new Period();
+    period.setName("period");
+    period.setProcessingSchedule(schedule);
+    period.setDescription("Test period");
+    period.setStartDate(LocalDate.of(2016, 1, 1));
+    period.setEndDate(LocalDate.of(2016, 2, 1));
+    periodRepository.save(period);
+  }
+
+  @After
+  public void cleanup() {
+    periodRepository.deleteAll();
+    scheduleRepository.deleteAll();
+  }
+
+  @Test
+  public void testGetTotalDifference() {
+    String response = restAssured.given().
+        pathParam("id", schedule.getId()).
+        when().
+        get("/api/schedules/{id}/difference").
+        then().
+        statusCode(200).
+        extract().asString();
+
+    assertTrue(response.contains("Period lasts 1 months and 0 days"));
+    assertThat(RAML_ASSERT_MESSAGE , restAssured.getLastReport(), RamlMatchers.hasNoViolations());
+  }
+}
