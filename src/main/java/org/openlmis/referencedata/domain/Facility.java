aeb64520c61809d5a47f6351a6e9daff1186b2c9
Szymon Kaczorowski <skaczorowski@soldevelo.com>
2016-07-20 16:19:25 +0200

    OLMIS-741.Create a simple requisition with additional stock information.

--- src/main/java/org/openlmis/referencedata/domain/Facility.java
+++ src/main/java/org/openlmis/referencedata/domain/Facility.java
@@ -7,7 +7,14 @@ import lombok.Setter;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "facilities", schema = "referencedata")
