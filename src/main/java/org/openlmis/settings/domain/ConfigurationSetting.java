2568c5964b17915d23b8abd62b8a53a3ddce23e2
Grzegorz Araminowicz <garaminowicz@soldevelo.com>
2016-07-21 14:26:41 +0200

    OLMIS-773: added classes to persist configuration settings

---
+++ src/main/java/org/openlmis/settings/domain/ConfigurationSetting.java
@@ -0,0 +1,26 @@
+package org.openlmis.settings.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "settings")
+@NoArgsConstructor
+public class ConfigurationSetting {
+
+  @Id
+  @Getter
+  @Setter
+  private String key;
+
+  @Column(nullable = false)
+  @Getter
+  @Setter
+  private String value;
+}
