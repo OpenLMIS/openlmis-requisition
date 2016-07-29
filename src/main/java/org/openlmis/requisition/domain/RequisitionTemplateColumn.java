5aab44e76c2f5c609369897a4452f0744fe34624
MaciejDudzik <mdudzik@soldevelo.com>
2016-07-29 11:34:32 +0200

    OLMIS-817: rename column headers/attributes

--- src/main/java/org/openlmis/requisition/domain/RequisitionTemplateColumn.java
+++ src/main/java/org/openlmis/requisition/domain/RequisitionTemplateColumn.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.persistence.Embeddable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Embeddable
 @NoArgsConstructor
@@ -29,6 +32,24 @@ public class RequisitionTemplateColumn {
   private Boolean canBeChangedByUser;
 
   private SourceType source;
+
+  /**
+   * Validate name of new label and change it if it's ok.
+   * @return result of validation
+   */
+  public boolean changeLabel(String labelName) {
+    if (validateString(labelName)) {
+      this.label = labelName;
+      return true;
+    }
+    return false;
+  }
+
+  private boolean validateString(String string) {
+    Pattern pattern = Pattern.compile("^[a-zA-z0-9]+[a-zA-Z0-9 ]+$");
+    Matcher matcher = pattern.matcher(string);
+    return matcher.find();
+  }
 
   /**
    * Allows changing visibility of specific column.
