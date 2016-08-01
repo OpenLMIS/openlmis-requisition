8bddf28ba10cc4c86b1ba018b74643ad70427cde
Grzegorz Araminowicz <garaminowicz@soldevelo.com>
2016-08-01 17:32:08 +0200

    OLMIS-773 FEOLMIS-111: ConfigurationSettingService fixes

---
+++ src/main/java/org/openlmis/settings/exception/ConfigurationSettingException.java
@@ -0,0 +1,8 @@
+package org.openlmis.settings.exception;
+
+public class ConfigurationSettingException extends Exception {
+
+  public ConfigurationSettingException(String message) {
+    super(message);
+  }
+}
