9ed93f03e4cbb2293b0dfe0a5f3bc95601015413
Szymon Kaczorowski <skaczorowski@soldevelo.com>
2016-08-04 11:31:34 +0200

    OLMIS-872. Removed unnecessary repositories tests.

--- src/main/java/org/openlmis/product/repository/ProductCategoryRepository.java
+++ src/main/java/org/openlmis/product/repository/ProductCategoryRepository.java
@@ -2,12 +2,10 @@ package org.openlmis.product.repository;
 
 import org.openlmis.product.domain.ProductCategory;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface ProductCategoryRepository 
     extends PagingAndSortingRepository<ProductCategory, UUID> {
 
-  ProductCategory findByCode(@Param("code") String code);
 }
