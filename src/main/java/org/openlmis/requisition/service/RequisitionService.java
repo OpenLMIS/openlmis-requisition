f152d3c14b02ed9e9883989569073702acc238f5
Szymon Kaczorowski <skaczorowski@soldevelo.com>
2016-08-09 13:29:47 +0200

    OLMIS-903. Moved Requisitions, RequisitionTemplates searches to repository layer.

--- src/main/java/org/openlmis/requisition/service/RequisitionService.java
+++ src/main/java/org/openlmis/requisition/service/RequisitionService.java
@@ -28,13 +28,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
 @Service
 public class RequisitionService {
   private static final String REQUISITION_NULL_MESSAGE = "requisition cannot be null";
@@ -57,9 +50,6 @@ public class RequisitionService {
 
   @Autowired
   private ConfigurationSettingService configurationSettingService;
-
-  @PersistenceContext
-  private EntityManager entityManager;
 
   /**
    * Initiated given requisition if possible.
@@ -213,39 +203,9 @@ public class RequisitionService {
                                               Period processingPeriod,
                                               SupervisoryNode supervisoryNode,
                                               RequisitionStatus requisitionStatus) {
-    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-    CriteriaQuery<Requisition> query = builder.createQuery(Requisition.class);
-    Root<Requisition> root = query.from(Requisition.class);
-    Predicate predicate = builder.conjunction();
-    if (facility != null) {
-      predicate = builder.and(predicate, builder.equal(root.get("facility"), facility));
-    }
-    if (program != null) {
-      predicate = builder.and(predicate, builder.equal(root.get("program"), program));
-    }
-    if (createdDateFrom != null) {
-      predicate = builder.and(predicate,
-          builder.greaterThanOrEqualTo(root.get("createdDate"), createdDateFrom));
-    }
-    if (createdDateTo != null) {
-      predicate = builder.and(predicate,
-          builder.lessThanOrEqualTo(root.get("createdDate"), createdDateTo));
-    }
-    if (processingPeriod != null) {
-      predicate = builder.and(predicate,
-              builder.equal(root.get("processingPeriod"), processingPeriod));
-    }
-    if (supervisoryNode != null) {
-      predicate = builder.and(predicate,
-              builder.equal(root.get("supervisoryNode"), supervisoryNode));
-    }
-    if (requisitionStatus != null) {
-      predicate = builder.and(predicate,
-              builder.equal(root.get("status"), requisitionStatus));
-    }
-
-    query.where(predicate);
-    return entityManager.createQuery(query).getResultList();
+    return requisitionRepository.searchRequisitions(
+            facility, program, createdDateFrom,
+            createdDateTo, processingPeriod,supervisoryNode,requisitionStatus);
   }
 
   /**
