3d1b526242a803b35df8e3e2b6c05456ba3ae212
plal <pawellal@gmail.com>
2016-08-10 13:36:21 +0200

    OLMIS-853: Added new version generator

--- src/main/java/org/openlmis/referencedata/utils/Version.java
+++ src/main/java/org/openlmis/referencedata/utils/Version.java
@@ -1,76 +1,54 @@
 package org.openlmis.referencedata.utils;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/*import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;*/
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
-/*import java.time.format.DateTimeFormatter;
-import java.util.Locale;*/
-
+/**
+ * Class containing version information.
+ */
 public class Version {
 
-  public static final String VERSION = "build/resources/main/version";
-  public static final String VERSION_TEMPLATE = "src/main/resources/version";
+  public static final String VERSION = "version.properties";
 
   @Getter
-  @Setter
-  private String service;
+  private String service = "service";
 
   @Getter
-  @Setter
-  private String build;
+  private String build = "${build}";
 
   @Getter
-  @Setter
-  private String branch;
+  private String branch = "${branch}";
 
   @Getter
-  @Setter
-  private String timeStamp;
+  private String timeStamp = "${time}";
 
   @Getter
-  @Setter
-  private String version;
+  private String version = "version";
 
-  Logger logger = LoggerFactory.getLogger(Version.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(Version.class);
 
   /**
-   * Allow displaying build information.
+   * Class constructor used to fill Version with data from version file.
    */
   public Version() {
-    File file = new File(VERSION);
-    if (!file.exists()) {
-      file = new File(VERSION_TEMPLATE);
-    }
-    BufferedReader reader = null;
-    try {
-      reader = new BufferedReader(new FileReader(file));
-      this.service = getValueFromLine(reader.readLine());
-      this.build = getValueFromLine(reader.readLine());
-      this.branch = getValueFromLine(reader.readLine());
-      this.timeStamp = getValueFromLine(reader.readLine());
-      this.version = getValueFromLine(reader.readLine());
-      reader.close();
-    } catch (FileNotFoundException ex) {
-      logger.error("Error opening non-existing file");
-    } catch (IOException ex) {
-      logger.error("Error reading line from file");
-    }
-  }
 
-  private String getValueFromLine(String line) {
-    return line.substring(line.indexOf(' ') + 1);
+    try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(VERSION)) {
+      if (inputStream != null) {
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        service = properties.getProperty("Service");
+        build = properties.getProperty("Build");
+        branch = properties.getProperty("Branch");
+        timeStamp = properties.getProperty("Timestamp");
+        version = properties.getProperty("Version");
+      }
+    } catch (IOException ex) {
+      LOGGER.error("Error loading version properties file");
+    }
   }
 }
