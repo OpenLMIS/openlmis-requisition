e0d040211286e72e9b63f96e4c42cb2a58532191
Pawel Muchowski <pmuchowski@soldevelo.com>
2016-09-14 14:01:08 +0200

    OLMIS-965: Write null values as empty string in csv

--- src/main/java/org/openlmis/fulfillment/utils/OrderCsvHelper.java
+++ src/main/java/org/openlmis/fulfillment/utils/OrderCsvHelper.java
@@ -123,6 +123,6 @@ public class OrderCsvHelper {
         columnValue = lineItemContext.getValue(orderFileColumn.getKeyPath());
         break;
     }
-    return columnValue;
+    return columnValue == null ? "" : columnValue;
   }
 }
