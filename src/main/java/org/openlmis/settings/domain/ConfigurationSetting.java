df4003bb7238d81cedac8136430c5860e9f22f92
Jakub Hopen <jhopen@soldevelo.com>
2016-09-20 16:04:30 +0200

    OLMIS-968 Fix table names to conform to the code style guide

--- src/main/java/org/openlmis/settings/domain/ConfigurationSetting.java
+++ src/main/java/org/openlmis/settings/domain/ConfigurationSetting.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "settings")
+@Table(name = "configuration_settings")
 @NoArgsConstructor
 @AllArgsConstructor
 public class ConfigurationSetting {
