1c9844ed98bfe69429be15e880f596fb04e42213
Łukasz Lewczyński <llewczynski@soldevelo.com>
2016-10-04 09:28:14 +0200

    OLMIS-820: Fixed IT test

--- src/main/java/org/openlmis/requisition/validate/RequisitionValidator.java
+++ src/main/java/org/openlmis/requisition/validate/RequisitionValidator.java
@@ -1,5 +1,6 @@
 package org.openlmis.requisition.validate;
 
+import org.openlmis.requisition.domain.AvailableRequisitionColumn;
 import org.openlmis.requisition.domain.Requisition;
 import org.openlmis.requisition.domain.RequisitionLineItem;
 import org.openlmis.requisition.domain.RequisitionTemplate;
@@ -10,7 +11,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.util.Map;
+
 import static org.apache.commons.lang.BooleanUtils.isFalse;
+import static org.apache.commons.lang.StringUtils.equalsIgnoreCase;
 import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Component
@@ -41,14 +45,14 @@ public class RequisitionValidator implements Validator {
     if (isEmpty(requisition.getRequisitionLineItems())) {
       errors.rejectValue(REQUISITION_LINE_ITEMS,
           "A requisitionLineItems" + VALUE_MUST_BE_ENTERED_NOTIFICATION);
+    } else {
+      RequisitionTemplate template = requisitionTemplateRepository.getTemplateForProgram(
+          requisition.getProgram()
+      );
+
+      requisition.getRequisitionLineItems()
+          .forEach(i -> validateRequisitionLineItem(errors, template, i));
     }
-
-    RequisitionTemplate template = requisitionTemplateRepository.getTemplateForProgram(
-        requisition.getProgram()
-    );
-
-    requisition.getRequisitionLineItems()
-        .forEach(i -> validateRequisitionLineItem(errors, template, i));
   }
 
   private void validateRequisitionLineItem(Errors errors, RequisitionTemplate template,
@@ -107,9 +111,7 @@ public class RequisitionValidator implements Validator {
   private boolean checkTemplate(Errors errors, RequisitionTemplate template,
                                 Object value, String field, String indicator) {
     if (null != indicator) {
-      RequisitionTemplateColumn column = template.getColumnsMap().values().stream()
-          .filter(c -> c.getColumnDefinition().getIndicator().equalsIgnoreCase(indicator))
-          .findFirst().orElse(null);
+      RequisitionTemplateColumn column = findColumn(template, indicator);
 
       if (null == column) {
         if (value != null) {
@@ -131,5 +133,35 @@ public class RequisitionValidator implements Validator {
     }
 
     return true;
+  }
+
+  private RequisitionTemplateColumn findColumn(RequisitionTemplate template, String indicator) {
+    if (null == template) {
+      return null;
+    }
+
+    Map<String, RequisitionTemplateColumn> columns = template.getColumnsMap();
+
+    if (null == columns || columns.isEmpty()) {
+      return null;
+    }
+
+    return columns.values().stream()
+        .filter(c -> findColumn(c, indicator))
+        .findFirst().orElse(null);
+  }
+
+  private boolean findColumn(RequisitionTemplateColumn column, String indicator) {
+    if (null == column) {
+      return false;
+    }
+
+    AvailableRequisitionColumn definition = column.getColumnDefinition();
+
+    if (null == definition) {
+      return false;
+    }
+
+    return equalsIgnoreCase(definition.getIndicator(), indicator);
   }
 }
