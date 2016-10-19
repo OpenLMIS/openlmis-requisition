7d3bbb28e20739c852aac924c4d489b7fec538ee
Jakub Hopen <jhopen@soldevelo.com>
2016-10-19 16:35:13 +0200

    OLMIS-990 update endpoing fix

--- src/main/java/org/openlmis/requisition/web/RequisitionController.java
+++ src/main/java/org/openlmis/requisition/web/RequisitionController.java
@@ -207,7 +207,9 @@ public class RequisitionController extends BaseController {
     }
 
     RequisitionStatus status = requisitionToUpdate.getStatus();
-    if (status != RequisitionStatus.APPROVED) {
+    if (status != RequisitionStatus.APPROVED
+        || status != RequisitionStatus.SKIPPED
+        || status != RequisitionStatus.RELEASED) {
       LOGGER.debug("Updating requisition with id: " + requisitionId);
 
       requisitionToUpdate.updateFrom(requisition,
