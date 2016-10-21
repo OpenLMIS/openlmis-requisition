46283d30138b956867bca4267f8db6924dc4eded
Łukasz Lewczyński <llewczynski@soldevelo.com>
2016-10-21 17:19:41 +0200

    OLMIS-1059: Fixed problem with code compilation (FEOLMIS-380)

--- src/main/java/org/openlmis/requisition/dto/RequisitionDto.java
+++ src/main/java/org/openlmis/requisition/dto/RequisitionDto.java
@@ -1,30 +1,78 @@
 package org.openlmis.requisition.dto;
 
+import org.openlmis.requisition.domain.Comment;
 import org.openlmis.requisition.domain.Requisition;
+import org.openlmis.requisition.domain.RequisitionLineItem;
 import org.openlmis.requisition.domain.RequisitionStatus;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RequisitionDto implements Requisition.Importer, Requisition.Exporter {
+  @Getter
+  @Setter
   private UUID id;
+
+  @Getter
+  @Setter
   private LocalDateTime createdDate;
+
+  @Setter
   private List<RequisitionLineItemDto> requisitionLineItems;
+
+  @Setter
   private List<CommentDto> comments;
+
+  @Getter
+  @Setter
   private FacilityDto facility;
+
+  @Getter
+  @Setter
   private ProgramDto program;
+
+  @Getter
+  @Setter
   private ProcessingPeriodDto processingPeriod;
+
+  @Getter
+  @Setter
   private RequisitionStatus status;
+
+  @Getter
+  @Setter
   private Boolean emergency;
+
+  @Getter
+  @Setter
   private UUID supplyingFacility;
+
+  @Getter
+  @Setter
   private UUID supervisoryNode;
 
+  @Override
+  public List<RequisitionLineItem.Importer> getRequisitionLineItems() {
+    return new ArrayList<>(
+        Optional.ofNullable(requisitionLineItems).orElse(Collections.emptyList())
+    );
+  }
+
+  @Override
+  public List<Comment.Importer> getComments() {
+    return new ArrayList<>(
+        Optional.ofNullable(comments).orElse(Collections.emptyList())
+    );
+  }
 }
