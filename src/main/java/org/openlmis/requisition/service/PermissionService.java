596d685ed71d8122f1ea0dc708e38f5487f74a8c
MikolajBasSoldevelo <mbas@soldevelo.com>
2017-01-05 15:17:41 +0100

    OLMIS-1413 - Change BooleanResultDto in Requisition Service to use generics

--- src/main/java/org/openlmis/requisition/service/PermissionService.java
+++ src/main/java/org/openlmis/requisition/service/PermissionService.java
@@ -1,8 +1,8 @@
 package org.openlmis.requisition.service;
 
 import org.openlmis.requisition.domain.Requisition;
-import org.openlmis.requisition.dto.BooleanResultDto;
 import org.openlmis.requisition.dto.ConvertToOrderDto;
+import org.openlmis.requisition.dto.ResultDto;
 import org.openlmis.requisition.dto.RightDto;
 import org.openlmis.requisition.dto.UserDto;
 import org.openlmis.requisition.exception.RequisitionException;
@@ -147,11 +147,11 @@ public class PermissionService {
       throws MissingPermissionException {
     UserDto user = authenticationHelper.getCurrentUser();
     RightDto right = authenticationHelper.getRight(rightName);
-    BooleanResultDto result = userReferenceDataService.hasRight(
+    ResultDto<Boolean> result = userReferenceDataService.hasRight(
         user.getId(), right.getId(), program, facility, warehouse
     );
 
-    if (null == result || !result.isResult()) {
+    if (null == result || result.getResult() == null || !result.getResult()) {
       throw new MissingPermissionException(rightName);
     }
   }
