5117ffa076a302da8c5fed79a0eebfd85a1adeb7
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-02-10 15:49:31 +0100

    OLMIS-1708: Moved BaseCommunicationService.buildUri to new class

---
+++ src/main/java/org/openlmis/utils/RequestHelper.java
@@ -0,0 +1,33 @@
+package org.openlmis.utils;
+
+import org.apache.commons.codec.Charsets;
+import org.openlmis.requisition.exception.EncodingException;
+import org.openlmis.requisition.service.RequestParameters;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UriUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+
+public final class RequestHelper {
+
+  private RequestHelper() {
+    throw new UnsupportedOperationException();
+  }
+
+  public static URI createUri(String url, RequestParameters parameters) {
+    UriComponentsBuilder builder = UriComponentsBuilder.newInstance().uri(URI.create(url));
+
+    parameters.forEach(e -> {
+      try {
+        builder.queryParam(e.getKey(),
+            UriUtils.encodeQueryParam(String.valueOf(e.getValue()), Charsets.UTF_8.name()));
+      } catch (UnsupportedEncodingException ex) {
+        throw new EncodingException(ex);
+      }
+    });
+
+    return builder.build(true).toUri();
+  }
+
+}
