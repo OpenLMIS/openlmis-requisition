4f311d4f07007a8a03bcfd19e7f8f8311c77d789
Paweł Nawrocki <pnawrocki@soldevelo.com>
2017-02-23 15:45:01 +0100

    OLMIS-308: Created GeographicZoneReferenceDataService class

---
+++ src/main/java/org/openlmis/requisition/service/referencedata/GeographicZoneReferenceDataService.java
@@ -0,0 +1,59 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+package org.openlmis.requisition.service.referencedata;
+
+import org.openlmis.requisition.dto.GeographicZoneDto;
+import org.openlmis.requisition.service.RequestParameters;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@Service
+public class GeographicZoneReferenceDataService
+    extends BaseReferenceDataService<GeographicZoneDto> {
+
+  @Override
+  protected String getUrl() {
+    return "/api/geographicZones/";
+  }
+
+  @Override
+  protected Class<GeographicZoneDto> getResultClass() {
+    return GeographicZoneDto.class;
+  }
+
+  @Override
+  protected Class<GeographicZoneDto[]> getArrayResultClass() {
+    return GeographicZoneDto[].class;
+  }
+
+  /**
+   * This method retrieves geographic zones filtered by geographic level and parent zone.
+   *
+   * @param level ID of geographic level
+   * @param parent ID of parent geographic zone
+   * @return List of matched geographic zones.
+   */
+  public Collection<GeographicZoneDto> search(UUID level, UUID parent) {
+    RequestParameters parameters = RequestParameters
+        .init()
+        .set("level", level)
+        .set("parent", parent);
+
+    return findAll("search", parameters);
+  }
+}
