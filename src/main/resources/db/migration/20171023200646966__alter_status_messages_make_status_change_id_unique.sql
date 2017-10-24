40a6eac5b18c44e3f4c1ec2504f90b586d39498f
joshzamor <josh.zamor@villagereach.org>
2017-10-24 13:26:44 -0700

    OLMIS-3409, de-dupe status messages by deleting them
    Similar to previous commit to de-dupe stock_adjustments, this one will delete duplicate duplicate status messages.

--- src/main/resources/db/migration/20171023200646966__alter_status_messages_make_status_change_id_unique.sql
+++ src/main/resources/db/migration/20171023200646966__alter_status_messages_make_status_change_id_unique.sql
@@ -1,1 +1,6 @@
+DELETE FROM status_messages t1
+  USING status_messages t2
+WHERE t1.ctid < t2.ctid
+  AND t1.statusChangeId = t2.statusChangeId;
+
 ALTER TABLE status_messages ADD CONSTRAINT status_change_id_unique UNIQUE (statusChangeId) DEFERRABLE INITIALLY DEFERRED;
