74fcc4d7c835780b327ae898047a850cea1c9c75
joshzamor <josh.zamor@villagereach.org>
2017-10-24 11:48:10 -0700

    OLMIS-3409, add simple delete dupes of stock adjustments
    Uses ctid which may or may not be correct, should be about as performant as we can get for postgres however.

--- src/main/resources/db/migration/20171020133555751__create_unique_index_on_stock_adjustments.sql
+++ src/main/resources/db/migration/20171020133555751__create_unique_index_on_stock_adjustments.sql
@@ -1,1 +1,7 @@
+DELETE FROM stock_adjustments t1
+  USING stock_adjustments t2
+WHERE t1.ctid < t2.ctid
+  AND t1.reasonid = t2.reasonid
+  AND t1.requisitionlineitemid = t2.requisitionlineitemid;
+
 CREATE UNIQUE INDEX req_line_reason ON stock_adjustments(reasonid, requisitionlineitemid);
