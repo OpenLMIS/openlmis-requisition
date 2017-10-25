b4a6ac192594b1b24acaf2e41ed352840822663e
Jakub Kondrat <jkondrat@soldevelo.com>
2017-10-25 14:51:45 +0200

    MW-682: Sum duplicate stock adjustment quantities before deleting them

--- src/main/resources/db/migration/20171020133555751__create_unique_index_on_stock_adjustments.sql
+++ src/main/resources/db/migration/20171020133555751__create_unique_index_on_stock_adjustments.sql
@@ -1,3 +1,15 @@
+UPDATE stock_adjustments t1
+SET quantity = t2.quantity
+FROM
+  (SELECT SUM(quantity) AS quantity,
+          requisitionlineitemid,
+          reasonid
+   FROM stock_adjustments
+   GROUP BY requisitionlineitemid,
+            reasonid) t2
+WHERE t1.reasonid = t2.reasonid
+  AND t1.requisitionlineitemid = t2.requisitionlineitemid;
+
 DELETE FROM stock_adjustments t1
   USING stock_adjustments t2
 WHERE t1.ctid < t2.ctid
