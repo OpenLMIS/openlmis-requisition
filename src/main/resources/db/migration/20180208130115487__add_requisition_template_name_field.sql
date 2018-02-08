8e61096ae04e709805029a46ea90d75e7d6c72bb
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-02-08 17:01:04 +0100

    OLMIS-4086: disable requisition template name unique

--- src/main/resources/db/migration/20180208130115487__add_requisition_template_name_field.sql
+++ src/main/resources/db/migration/20180208130115487__add_requisition_template_name_field.sql
@@ -1,2 +1,3 @@
 ALTER TABLE ONLY requisition_templates ADD COLUMN name character varying(255) NOT NULL;
-CREATE UNIQUE INDEX requisition_template_name_unique_idx ON requisition_templates (name)
+-- disable for now
+-- CREATE UNIQUE INDEX requisition_template_name_unique_idx ON requisition_templates (name)
