78c3312d8f2ceab5ca0e53190357503d5ce59821
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-02-12 09:08:18 +0100

    Revert "Revert "OLMIS-4086: Added legacy flag to requisition template""
    This reverts commit 2b832c87fd62489b7e9b4a60f38cb247bd2671bb.

---
+++ src/main/resources/db/migration/20180208130115487__add_requisition_template_name_and_legacy_fields.sql
@@ -0,0 +1,25 @@
+ALTER TABLE ONLY requisition_templates ADD COLUMN legacy boolean NOT NULL DEFAULT FALSE;
+ALTER TABLE ONLY requisition_templates ADD COLUMN name character varying(255) NOT NULL;
+
+-- set legacy flag for old templates
+UPDATE requisition_templates
+SET legacy = true;
+
+UPDATE requisition_templates
+SET legacy = false
+WHERE id IN (
+  SELECT id
+  FROM requisition.requisition_templates AS t1
+  WHERE t1.createdDate = (
+    SELECT max(t2.createdDate)
+    FROM requisition.requisition_templates AS t2
+    WHERE t1.programId = t2.programId
+  )
+);
+
+-- the unique index should work only on current templates. It should be possible to have several
+-- legacy templates with the same name because of current template structure (we create a new
+-- template if there is at least one requisition connected with the current template)
+CREATE UNIQUE INDEX requisition_template_name_unique_idx
+ON requisition_templates (name)
+WHERE legacy IS FALSE;
--- src/main/resources/db/migration/20180208130115487__add_requisition_template_name_field.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE ONLY requisition_templates ADD COLUMN name character varying(255) NOT NULL;
--- disable for now
--- CREATE UNIQUE INDEX requisition_template_name_unique_idx ON requisition_templates (name)
