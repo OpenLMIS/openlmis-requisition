c8b13b959b392a293b71b07f85e4c235132cd891
Chongsun Ahn <cahn922@yahoo.com>
2018-03-14 00:12:50 -0700

    OLMIS-4239 Fix faulty migration
    New column added which cannot be nullable, but no default was given.

--- src/main/resources/db/migration/20180127203405321__added_stock_configuration_to_requisition_template.sql
+++ src/main/resources/db/migration/20180127203405321__added_stock_configuration_to_requisition_template.sql
@@ -1,5 +1,5 @@
 ALTER TABLE requisition_templates
-    ADD populateStockOnHandFromStockCards boolean NOT NULL;
+    ADD populateStockOnHandFromStockCards boolean DEFAULT false NOT NULL;
 
 INSERT INTO requisition.available_requisition_column_sources (columnId, value)
     VALUES ('752cda76-0db5-4b6e-bb79-0f531ab78e2c', 'STOCK_CARDS'); -- stockOnHand
