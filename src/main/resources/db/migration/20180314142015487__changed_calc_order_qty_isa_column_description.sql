ab6f956e8322cc3ed9d41014df970791686b1323
Łukasz Lewczyński <lukaslew@users.noreply.github.com>
2018-03-15 09:23:16 +0100

    OLMIS-4220: added missing dot in description

--- src/main/resources/db/migration/20180314142015487__changed_calc_order_qty_isa_column_description.sql
+++ src/main/resources/db/migration/20180314142015487__changed_calc_order_qty_isa_column_description.sql
@@ -1,3 +1,3 @@
 UPDATE requisition.available_requisition_columns
-SET definition = 'Calculated Order Quantity ISA is based on an ISA configured by commodity type, and several trade items may fill for one commodity type'
+SET definition = 'Calculated Order Quantity ISA is based on an ISA configured by commodity type, and several trade items may fill for one commodity type.'
 WHERE name = 'calculatedOrderQuantityIsa';
