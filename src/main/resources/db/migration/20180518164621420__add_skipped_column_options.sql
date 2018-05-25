7ec78b19d23d9fdb64eb9d955f3bd02a2b2f29e2
Elias Muluneh <eliasmu@gmail.com>
2018-05-25 09:34:53 -0400

    OLMIS-4792: Make dropdown in additional options column of requisition template translatable.
    Change the labels of available requisition column options from human readable
    text to translatable key.

--- src/main/resources/db/migration/20180518164621420__add_skipped_column_options.sql
+++ src/main/resources/db/migration/20180518164621420__add_skipped_column_options.sql
@@ -3,9 +3,9 @@ VALUES ('c6dffdee-3813-40d9-8737-f531d5adf420', 'PREVIOUS_REQUISITION');
 
 INSERT INTO requisition.available_requisition_column_options (id, optionlabel, optionname, columnid)
 VALUES
-  ('17d6e860-a746-4500-a0fa-afc84d799dca', 'Disable skipped line items', 'disableSkippedLineItems',
+  ('17d6e860-a746-4500-a0fa-afc84d799dca', 'requisitionConstants.disableSkippedLineItems', 'disableSkippedLineItems',
    'c6dffdee-3813-40d9-8737-f531d5adf420'),
-  ('488e6882-563d-4b69-b7eb-fd59e7772a41', 'Hide skipped line items', 'hideSkippedLineItems',
+  ('488e6882-563d-4b69-b7eb-fd59e7772a41', 'requisitionConstants.hideSkippedLineItems', 'hideSkippedLineItems',
    'c6dffdee-3813-40d9-8737-f531d5adf420');
 
 -- Migrate existing implementations to the default 'skipped' column behavior.
@@ -13,3 +13,17 @@ VALUES
 UPDATE requisition.columns_maps
 SET requisitioncolumnoptionid = '17d6e860-a746-4500-a0fa-afc84d799dca'
 WHERE name = 'skipped' and requisitioncolumnoptionid IS NULL;
+
+
+-- allow the user
+UPDATE requisition.available_requisition_column_options
+    SET optionLabel = 'requisitionConstants.newPatientCount'
+    WHERE optionName = 'newPatientCount';
+
+UPDATE requisition.available_requisition_column_options
+    SET optionLabel = 'requisitionConstants.dispensingUnitsForNewPatients'
+    WHERE optionName = 'dispensingUnitsForNewPatients';
+
+UPDATE requisition.available_requisition_column_options
+    SET optionLabel = 'requisitionConstants.default'
+    WHERE optionName = 'default';
