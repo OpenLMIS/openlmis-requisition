8864e35aed8bf34b43e45ea7f319cf713a864da0
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-08-03 09:28:32 +0200

    OLMIS-5148: Extended migration to add column to all req templates

--- src/main/resources/db/migration/20180706200019454__add_additional_quantity_required_column.sql
+++ src/main/resources/db/migration/20180706200019454__add_additional_quantity_required_column.sql
@@ -1,3 +1,4 @@
+-- add a new column to the system
 INSERT INTO requisition.available_requisition_columns
   (id, name, label, indicator,
    mandatory, isDisplayRequired, canChangeOrder, canBeChangedByUser, columnType, definition)
@@ -8,6 +9,18 @@ VALUES
 INSERT INTO requisition.available_requisition_column_sources (columnId, value)
 VALUES ('90a84767-1009-4d8e-be13-f006fce2a002', 'USER_INPUT');
 
+-- update the requisition line item structure
 ALTER TABLE requisition.requisition_line_items
   ADD COLUMN additionalQuantityRequired INTEGER;
 
+-- add the new column to all requisition templates
+INSERT INTO
+	requisition.columns_maps(requisitiontemplateid, requisitioncolumnid, definition, displayorder, indicator, isdisplayed, label, name, requisitioncolumnoptionid, source, key, tag)
+SELECT
+	t.id, a.id, a.definition, c.count + 1, a.indicator, false, a.label, a.name, NULL, 0, a.name, NULL
+FROM
+	requisition.requisition_templates AS t
+	INNER JOIN (SELECT requisitiontemplateid, count(*) FROM requisition.columns_maps GROUP BY requisitiontemplateid) AS c ON c.requisitiontemplateid = t.id,
+	requisition.available_requisition_columns AS a
+WHERE
+	a.name = 'additionalQuantityRequired';
