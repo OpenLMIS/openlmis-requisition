86dda464bf9cfc119a17db9db093ff5c9e120699
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2018-10-26 15:41:17 +0200

    OLMIS-5494: fixed available columns names for product code and name

---
+++ src/main/resources/db/migration/20181026152017631__fixed_available_columns_names.sql
@@ -0,0 +1,2 @@
+UPDATE requisition.available_requisition_columns set name = 'orderable.productCode' where id = 'bde01507-3837-47b7-ae08-cec92c0c3cd2';
+UPDATE requisition.available_requisition_columns set name = 'orderable.fullProductName' where id = 'e53e80de-fc63-4ecb-b6b2-ef376b34c926';
