a7b7f67cb8a982c300e82366923cd85e882d06e4
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2018-10-29 13:09:29 +0100

    OLMIS-5494: fixed column id in migration

--- src/main/resources/db/migration/20181026152017631__fixed_available_columns_names.sql
+++ src/main/resources/db/migration/20181026152017631__fixed_available_columns_names.sql
@@ -1,3 +1,3 @@
 UPDATE requisition.available_requisition_columns set name = 'orderable.productCode' where id = 'bde01507-3837-47b7-ae08-cec92c0c3cd2';
 UPDATE requisition.available_requisition_columns set name = 'orderable.fullProductName' where id = 'e53e80de-fc63-4ecb-b6b2-ef376b34c926';
-UPDATE requisition.available_requisition_columns set name = 'orderable.dispensable.displayUnit' where id = 'e53e80de-fc63-4ecb-b6b2-ef376b34c926';
+UPDATE requisition.available_requisition_columns set name = 'orderable.dispensable.displayUnit' where id = '61e6d059-10ef-40c4-a6e3-fa7b9ad741ec';
