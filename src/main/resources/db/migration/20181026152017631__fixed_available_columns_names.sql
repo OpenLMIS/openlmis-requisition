9961e6e0d2b1372689afc9ba03841b7c1eba4d75
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2018-10-29 11:31:54 +0100

    OLMIS-5494: fixed name for Unit of issue column

--- src/main/resources/db/migration/20181026152017631__fixed_available_columns_names.sql
+++ src/main/resources/db/migration/20181026152017631__fixed_available_columns_names.sql
@@ -1,2 +1,3 @@
 UPDATE requisition.available_requisition_columns set name = 'orderable.productCode' where id = 'bde01507-3837-47b7-ae08-cec92c0c3cd2';
 UPDATE requisition.available_requisition_columns set name = 'orderable.fullProductName' where id = 'e53e80de-fc63-4ecb-b6b2-ef376b34c926';
+UPDATE requisition.available_requisition_columns set name = 'orderable.dispensable.displayUnit' where id = 'e53e80de-fc63-4ecb-b6b2-ef376b34c926';
