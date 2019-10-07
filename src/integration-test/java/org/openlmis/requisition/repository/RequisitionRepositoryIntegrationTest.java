fd1e784983f602b660cdbad698613a4d37af9a63
Łukasz Lewczyński <llewczynski@soldevelo.com>
2019-10-07 13:36:24 +0200

    OLMIS-6619: Update comments in tests

--- src/integration-test/java/org/openlmis/requisition/repository/RequisitionRepositoryIntegrationTest.java
+++ src/integration-test/java/org/openlmis/requisition/repository/RequisitionRepositoryIntegrationTest.java
@@ -565,7 +565,7 @@ public class RequisitionRepositoryIntegrationTest
     matchingRequisition1.setSupervisoryNodeId(supervisoryNodeId);
     matchingRequisition1.setStatus(RequisitionStatus.AUTHORIZED);
 
-    // simulation that the requisition has been rejected 4 times
+    // simulation that the requisition has been rejected 3 times
     matchingRequisition1
         .getStatusChanges()
         .add(new StatusChangeDataBuilder()
@@ -599,7 +599,7 @@ public class RequisitionRepositoryIntegrationTest
     matchingRequisition2.setSupervisoryNodeId(supervisoryNodeId);
     matchingRequisition2.setStatus(RequisitionStatus.IN_APPROVAL);
 
-    // simulation that the requisition has been rejected 3 times
+    // simulation that the requisition has been rejected 2 times
     matchingRequisition2
         .getStatusChanges()
         .add(new StatusChangeDataBuilder()
@@ -647,7 +647,7 @@ public class RequisitionRepositoryIntegrationTest
     matchingRequisition1.setEmergency(false);
     matchingRequisition1.getStatusChanges().clear();
 
-    // simulation that the requisition has been rejected 2 times
+    // simulation that the requisition has been rejected 1 times
     matchingRequisition1
         .getStatusChanges()
         .add(new StatusChangeDataBuilder()
