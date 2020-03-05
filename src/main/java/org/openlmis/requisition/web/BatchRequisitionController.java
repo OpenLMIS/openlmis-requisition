9876adcf498c59e3b5cb8989b9703bc1e3c7dcd6
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2020-03-05 15:59:39 +0100

    OLMIS-6777: Added Transactional annotation to BatchRequisitionController

--- src/main/java/org/openlmis/requisition/web/BatchRequisitionController.java
+++ src/main/java/org/openlmis/requisition/web/BatchRequisitionController.java
@@ -78,6 +78,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -87,6 +88,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @Controller
+@Transactional
 @SuppressWarnings("PMD.TooManyMethods")
 public class BatchRequisitionController extends BaseRequisitionController {
 
