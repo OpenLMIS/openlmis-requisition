27ac0887f1fdd61565e0febd38634d393521d33f
Szymon Radziszewski <117299684+sradziszewski@users.noreply.github.com>
2023-07-17 15:41:51 +0200

    TZUP-477: Changed Individual Monthly Requirement column source (#79)
    Added new columns to demo-data
    Optimized the migration script

--- src/main/resources/db/migration/20230705133304288__add_columns_for_tb_monthly.sql
+++ src/main/resources/db/migration/20230705133304288__add_columns_for_tb_monthly.sql
@@ -3,17 +3,17 @@ INSERT INTO requisition.available_requisition_columns
    mandatory, isDisplayRequired, canChangeOrder, canBeChangedByUser, columnType, definition)
 VALUES
   ('ef7c65c6-1be3-410b-9a2e-8b9f68221e82', 'numberOfPatientsOnTreatmentNextMonth', 'No of Patients on Treatment next month (C)', 'TC',
-   false, false, true, true, 'NUMERIC', 'The number of patients who will be treated in the next month'),
+   false, false, true, true, 'NUMERIC', 'The number of patients who will be treated in the next month.'),
   ('7563a92f-2c07-4b09-a88e-d0faa0baee93', 'totalRequirement', 'Total Requirement (E) E = C * D', 'TE',
-      false, false, true, true, 'NUMERIC', 'Product requirement for all patients'),
+      false, false, true, true, 'NUMERIC', 'Product requirement for all patients.'),
   ('d58e83a3-78ee-4657-b3d9-7b1adc983837', 'totalQuantityNeededByHf', 'Total Quantity Needed by HF (F) F = E * 2', 'TF',
-     false, false, true, true, 'NUMERIC', 'Total quantity of product needed by Health Facility'),
+     false, false, true, true, 'NUMERIC', 'Total quantity of product needed by Health Facility.'),
   ('421cd415-f127-4683-a396-a10a7bfdc384', 'quantityToIssue', 'Quantity to issue (G) G = F - A', 'TG',
-     false, false, true, true, 'NUMERIC', 'Number of products to be issued'),
+     false, false, true, true, 'NUMERIC', 'Number of products to be issued.'),
   ('d173872d-f5e7-48a2-b0a7-c7fcae4208d4', 'convertedQuantityToIssue', 'Quantity to issue (converted to unit of measure) G/U', 'TH',
-     false, false, true, true, 'NUMERIC', 'Quantity to issue expressed in the units in which it is measured'),
+     false, false, true, true, 'NUMERIC', 'Quantity to issue expressed in the units in which it is measured.'),
   ('3efdf855-cc03-4ccc-97f2-60108bc4b7a3', 'programOrderables.dosesperpatient', 'Individual Monthly Requirement (D)', 'TD',
-     false, false, true, true, 'NUMERIC', 'The amount of product for single patient');
+     false, false, true, true, 'NUMERIC', 'The amount of product for single patient.');
 
 INSERT INTO requisition.available_requisition_column_sources (columnId, value) VALUES
 ('ef7c65c6-1be3-410b-9a2e-8b9f68221e82', 'CALCULATED'),
@@ -22,7 +22,7 @@ INSERT INTO requisition.available_requisition_column_sources (columnId, value) V
 ('d58e83a3-78ee-4657-b3d9-7b1adc983837', 'CALCULATED'),
 ('421cd415-f127-4683-a396-a10a7bfdc384', 'CALCULATED'),
 ('d173872d-f5e7-48a2-b0a7-c7fcae4208d4', 'CALCULATED'),
-('3efdf855-cc03-4ccc-97f2-60108bc4b7a3', 'REFERENCE_DATA');
+('3efdf855-cc03-4ccc-97f2-60108bc4b7a3', 'CALCULATED');
 
 
 ALTER TABLE requisition.requisition_line_items
@@ -33,56 +33,27 @@ ALTER TABLE requisition.requisition_line_items
   ADD COLUMN convertedQuantityToIssue INTEGER;
 
 -- add the new column to all requisition templates
-INSERT INTO
-	requisition.columns_maps(requisitiontemplateid, requisitioncolumnid, definition, displayorder, indicator, isdisplayed, label, name, requisitioncolumnoptionid, source, key, tag)
-SELECT
-	t.id, a.id, a.definition, 7, a.indicator, false, a.label, a.name, NULL, 1, a.name, NULL
+INSERT INTO requisition.columns_maps (requisitiontemplateid, requisitioncolumnid, definition, displayorder, indicator,
+isdisplayed, label, name, requisitioncolumnoptionid, source, key, tag)
+SELECT t.id, a.id, a.definition,
+  CASE a.name
+    WHEN 'numberOfPatientsOnTreatmentNextMonth' THEN 7
+    WHEN 'programOrderables.dosesperpatient' THEN 8
+    WHEN 'totalRequirement' THEN 9
+    WHEN 'totalQuantityNeededByHf' THEN 10
+    WHEN 'quantityToIssue' THEN 11
+    WHEN 'convertedQuantityToIssue' THEN 12
+  END AS displayorder, a.indicator, false, a.label, a.name, NULL, 1, a.name, NULL
 FROM
-	requisition.requisition_templates AS t
-	INNER JOIN (SELECT requisitiontemplateid, count(*) FROM requisition.columns_maps GROUP BY requisitiontemplateid) AS c ON c.requisitiontemplateid = t.id
-	INNER JOIN requisition.available_requisition_columns AS a ON a.name = 'numberOfPatientsOnTreatmentNextMonth';
-
-INSERT INTO
-	requisition.columns_maps(requisitiontemplateid, requisitioncolumnid, definition, displayorder, indicator, isdisplayed, label, name, requisitioncolumnoptionid, source, key, tag)
-SELECT
-	t.id, a.id, a.definition, 8, a.indicator, false, a.label, a.name, NULL, 2, a.name, NULL
-FROM
-	requisition.requisition_templates AS t
-	INNER JOIN (SELECT requisitiontemplateid, count(*) FROM requisition.columns_maps GROUP BY requisitiontemplateid) AS c ON c.requisitiontemplateid = t.id
-	INNER JOIN requisition.available_requisition_columns AS a ON a.name = 'programOrderables.dosesperpatient';
-
-INSERT INTO
-	requisition.columns_maps(requisitiontemplateid, requisitioncolumnid, definition, displayorder, indicator, isdisplayed, label, name, requisitioncolumnoptionid, source, key, tag)
-SELECT
-	t.id, a.id, a.definition, 9, a.indicator, false, a.label, a.name, NULL, 1, a.name, NULL
-FROM
-	requisition.requisition_templates AS t
-	INNER JOIN (SELECT requisitiontemplateid, count(*) FROM requisition.columns_maps GROUP BY requisitiontemplateid) AS c ON c.requisitiontemplateid = t.id
-	INNER JOIN requisition.available_requisition_columns AS a ON a.name = 'totalRequirement';
-
-INSERT INTO
-	requisition.columns_maps(requisitiontemplateid, requisitioncolumnid, definition, displayorder, indicator, isdisplayed, label, name, requisitioncolumnoptionid, source, key, tag)
-SELECT
-	t.id, a.id, a.definition, 10, a.indicator, false, a.label, a.name, NULL, 1, a.name, NULL
-FROM
-	requisition.requisition_templates AS t
-	INNER JOIN (SELECT requisitiontemplateid, count(*) FROM requisition.columns_maps GROUP BY requisitiontemplateid) AS c ON c.requisitiontemplateid = t.id
-	INNER JOIN requisition.available_requisition_columns AS a ON a.name = 'totalQuantityNeededByHf';
-
-INSERT INTO
-	requisition.columns_maps(requisitiontemplateid, requisitioncolumnid, definition, displayorder, indicator, isdisplayed, label, name, requisitioncolumnoptionid, source, key, tag)
-SELECT
-	t.id, a.id, a.definition, 11, a.indicator, false, a.label, a.name, NULL, 1, a.name, NULL
-FROM
-	requisition.requisition_templates AS t
-	INNER JOIN (SELECT requisitiontemplateid, count(*) FROM requisition.columns_maps GROUP BY requisitiontemplateid) AS c ON c.requisitiontemplateid = t.id
-	INNER JOIN requisition.available_requisition_columns AS a ON a.name = 'quantityToIssue';
-
-INSERT INTO
-	requisition.columns_maps(requisitiontemplateid, requisitioncolumnid, definition, displayorder, indicator, isdisplayed, label, name, requisitioncolumnoptionid, source, key, tag)
-SELECT
-	t.id, a.id, a.definition, 12, a.indicator, false, a.label, a.name, NULL, 1, a.name, NULL
-FROM
-	requisition.requisition_templates AS t
-	INNER JOIN (SELECT requisitiontemplateid, count(*) FROM requisition.columns_maps GROUP BY requisitiontemplateid) AS c ON c.requisitiontemplateid = t.id
-	INNER JOIN requisition.available_requisition_columns AS a ON a.name = 'convertedQuantityToIssue';
+  requisition.requisition_templates AS t
+  INNER JOIN (
+    SELECT requisitiontemplateid, count(*) FROM requisition.columns_maps GROUP BY requisitiontemplateid
+  ) AS c ON c.requisitiontemplateid = t.id
+  INNER JOIN requisition.available_requisition_columns AS a ON a.name IN (
+    'numberOfPatientsOnTreatmentNextMonth',
+    'programOrderables.dosesperpatient',
+    'totalRequirement',
+    'totalQuantityNeededByHf',
+    'quantityToIssue',
+    'convertedQuantityToIssue'
+  );
